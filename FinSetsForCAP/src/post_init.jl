f056b3240ac319d77216d6a2a918fad50b59ba9e
Kamal Saleh <kamal.saleh@web.de>
2025-08-06 14:20:18 +0200

    move non-standard initialization code from FinSetsForCAP/src/FinSetsForCAP.jl
    into a separate post_init.jl file

---
+++ FinSetsForCAP/src/post_init.jl
@@ -0,0 +1,39 @@
+
+# Iterator in FinSets
+function iterate(M::IsObjectInCategoryOfFiniteSets.abstract_type)
+	iterate( AsList( M ) )
+end
+
+function iterate(M::IsObjectInCategoryOfFiniteSets.abstract_type, state)
+	iterate( AsList( M ), state )
+end
+
+# CallFuncList in FinSets
+function (phi::IsMorphismInCategoryOfFiniteSets.abstract_type)(x)
+	CallFuncList( phi, [ x ] )
+end
+
+# convenience converting Int to BigInt in SkeletalFinSets
+
+##
+function FinSet(n::Int)
+    FinSet(BigInt(n));
+end
+
+##
+function MapOfFinSets(s::IsObjectInSkeletalCategoryOfFiniteSets.abstract_type, G::Vector{Int}, t::IsObjectInSkeletalCategoryOfFiniteSets.abstract_type)
+	MapOfFinSets( s, Vector{BigInt}(G), t );
+end
+
+function MapOfFinSets(s::IsObjectInSkeletalCategoryOfFiniteSets.abstract_type, G::UnitRange{Int}, t::IsObjectInSkeletalCategoryOfFiniteSets.abstract_type)
+	MapOfFinSets( s, UnitRange{BigInt}(G), t );
+end
+
+function MapOfFinSets(s::IsObjectInSkeletalCategoryOfFiniteSets.abstract_type, G::StepRange{Int}, t::IsObjectInSkeletalCategoryOfFiniteSets.abstract_type)
+	MapOfFinSets( s, StepRange{BigInt}(G), t );
+end
+
+# CallFuncList in SkeletalFinSets
+function (phi::IsMorphismInSkeletalCategoryOfFiniteSets.abstract_type)(x)
+	CallFuncList( phi, [ x ] )
+end
