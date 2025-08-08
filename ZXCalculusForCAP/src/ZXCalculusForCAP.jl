4a53c1f88ddbdaf5f55d5657d0a4647f1382dff3
Kamal Saleh <kamal.saleh@web.de>
2025-08-08 19:49:11 +0200

    move non-standard initialization code from ZXCalculusForCAP/src/ZXCalculusForCAP.jl
    into a separate post_init.jl file.

--- ZXCalculusForCAP/src/ZXCalculusForCAP.jl
+++ ZXCalculusForCAP/src/ZXCalculusForCAP.jl
@@ -16,16 +16,6 @@ import JSON3
 
 include("init.jl")
 
-# convenience converting Int to BigInt
-
-##
-function ObjectConstructor(cat::IsCategoryOfZXDiagrams.abstract_type, n::Int)
-    ObjectConstructor(cat, BigInt(n));
-end
-
-##
-function MorphismConstructor(s::IsObjectInCategoryOfZXDiagrams.abstract_type, graph::Tuple{Vector{String}, Vector{Vector{Int64}}}, t::IsObjectInCategoryOfZXDiagrams.abstract_type)
-	MorphismConstructor(s, [graph[1], map(edge -> [ BigInt( edge[1] ), BigInt( edge[2] ) ], graph[2])], t)
-end
+include("post_init.jl")
 
 end # module ZXCalculusForCAP
